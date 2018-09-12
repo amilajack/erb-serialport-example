76ffc8e2b4a9c636b391bfa66ad7d197f9cd5ef7
Amila Welihinda <amilajack@gmail.com>
2018-09-11 19:22:37 -0700

    Initial serialport commit

--- app/index.js
+++ app/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import SerialPort from 'serialport/test';
+import serialport from 'serialport';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
 import './app.global.css';
+
+const MockBinding = SerialPort.Binding;
+
+console.log(serialport);
+
+// Create a port and enable the echo and recording.
+MockBinding.createPort('/dev/ROBOT', { echo: true, record: true });
+const port = new SerialPort('/dev/ROBOT');
+console.log(port);
 
 const store = configureStore();
 
